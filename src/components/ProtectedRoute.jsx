7af1e4458a0777ef6ff92179b9252af363aa0ea1
IamSnowLeopard <146442423+IamSnowLeopard@users.noreply.github.com>
2024-07-02 20:55:10 -0400

    Bug fix

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
+const ProtectedRoute = ({ element: Component, ...rest }) => {
   const isAuthenticated = !!localStorage.getItem("token"); // Check if token exists
 
   return (
     <Route
       {...rest}
-      render={(props) =>
-        isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
-      }
+      element={isAuthenticated ? <Component /> : <Navigate to="/login" />}
     />
   );
 };
